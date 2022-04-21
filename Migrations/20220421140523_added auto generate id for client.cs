3a25a943b78418034e29d015a40bf6055525f258
omarkar101 <omar.kar101@gmail.com>
2022-04-21 17:05:48 +0300

    added some pages

---
+++ Migrations/20220421140523_added auto generate id for client.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace barber_shops.Migrations
+{
+    public partial class addedautogenerateidforclient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Clients");
+        }
+    }
+}
--- Models/Client.cs
+++ Models/Client.cs
@@ -1,19 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace barber_shops.Models;
 
 public class Client
 {
-
     [Key]
     [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     [Required]
     public string FirstName {get; set;} = null!;
 
     [Required]
+    public string LastName {get;set;} = null!;
+
+    [Required]
     [DataType(DataType.PhoneNumber)]
     public string PhoneNumber {get;set;} = null!;
 
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email {get;set;} = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password {get;set;} = null!;
 }

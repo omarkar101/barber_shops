5ddc02f3f36e3b91329ad12162e8c5f95196207c
JadAwky <73030738+JadAwky@users.noreply.github.com>
2022-04-21 14:48:57 +0300

    Started with Home Page frontend

--- Models/Barber.cs
+++ Models/Barber.cs
@@ -7,7 +7,7 @@ public class Barber
 
     [Key]
     [Required]
-    public int Id { get; set; }
+    public int Id {get; set;} = 0;
 
     [Required]
     public string FirstName {get; set;} = null!;
@@ -16,7 +16,19 @@ public class Barber
     public string LastName {get;set;} = null!;
 
     [Required]
+    public string Location {get; set;} = null!;
+
+    [Required]
     [DataType(DataType.PhoneNumber)]
     public string PhoneNumber {get;set;} = null!;
+
+    [Required]
+    public string IMG {get; set;} = null!;
+
+    [Required]
+    public int averagePrice {get; set;} = 0;
+
+    [Required]
+    public int Rating {get; set;} = 0;
 
 }

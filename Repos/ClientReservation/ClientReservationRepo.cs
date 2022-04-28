fde65f80069e6a5963a6fca6759af44bbc0840ee
Abbass Hoteit <Akh24@mail.aub.edu>
2022-04-29 01:03:24 +0300

    Reservation

--- Repos/ClientReservation/ClientReservationRepo.cs
+++ Repos/ClientReservation/ClientReservationRepo.cs
@@ -1,14 +1,42 @@
+using barber_shops.Data;
 using barber_shops.Models;
 namespace barber_shops.Repos;
 
-public class ClientReservationsRepo : IClientReservationsRepo
+public class ClientReservationsRepo: IClientReservationsRepo
 {
-    // public IEnumerable<ClientReservation> GetClientReservations(string clientEmail) {
+    private readonly AppDbContext _context;
 
-    // }
-    // // ClientReservation? GetClientReservationByID(string id)
-    // ClientReservation? GetClientReservationByBarberAndClientEmails(string clientEmail, string BarberEmail);
-    // void AddClientReservation(ClientReservation ClientReservation);
-    // void DeleteClientReservation(ClientReservation ClientReservation);
-    // void UpdateClientReservation(string id, ClientReservation ClientReservation);
+    public ClientReservationsRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IEnumerable<ClientReservation> GetClientReservations() {
+        return _context.ClientReservations.OrderBy(c => c.Id).ToList();
+    }
+    public ClientReservation? GetClientReservationByBarberAndClientEmails(string clientEmail, string barberEmail){
+        return _context.ClientReservations.Where(c=>c.ClientEmail.Equals(clientEmail) && c.BarberEmail.Equals(barberEmail)).FirstOrDefault();
+    }
+    public ClientReservation? GetClientReservationByID(string id){
+        return _context.ClientReservations.Where(c=>c.Id==id).FirstOrDefault();
+    }
+    public void AddClientReservation(ClientReservation clientReservation){
+        _context.ClientReservations.Add(clientReservation);
+        _context.SaveChanges();
+    }
+    public void DeleteClientReservation(ClientReservation clientReservation){
+        var clientToDelete = GetClientReservationByID(clientReservation.Id);
+            if (clientToDelete != null)
+            {
+                _context.ClientReservations.Remove(clientReservation);
+                _context.SaveChanges();
+            }
+    }
+    public void UpdateClientReservation(string id,ClientReservation clientReservation){
+        var clientToUpdate = GetClientReservationByID(id);
+        if (clientToUpdate != null)
+        {
+            clientToUpdate.BarberEmail = clientReservation.BarberEmail;
+            _context.SaveChanges();
+        }
+    }
 }
--- Repos/ClientReservation/IClientReservationRepo.cs
+++ Repos/ClientReservation/IClientReservationRepo.cs
@@ -3,10 +3,10 @@ namespace barber_shops.Repos;
 
 public interface IClientReservationsRepo
 {
-    // IEnumerable<ClientReservation> GetClientReservations(string clientEmail);
-    // ClientReservation? GetClientReservationByID(string id);
-    // ClientReservation? GetClientReservationByBarberAndClientEmails(string clientEmail, string BarberEmail);
-    // void AddClientReservation(ClientReservation ClientReservation);
-    // void DeleteClientReservation(ClientReservation ClientReservation);
-    // void UpdateClientReservation(string id, ClientReservation ClientReservation);
+    IEnumerable<ClientReservation> GetClientReservations();
+    ClientReservation? GetClientReservationByID(string id);
+    ClientReservation? GetClientReservationByBarberAndClientEmails(string clientEmail, string BarberEmail);
+    void AddClientReservation(ClientReservation ClientReservation);
+    void DeleteClientReservation(ClientReservation ClientReservation);
+    void UpdateClientReservation(string id, ClientReservation ClientReservation);
 }
